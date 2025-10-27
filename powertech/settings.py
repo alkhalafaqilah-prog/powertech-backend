ba7244b58ebc45ec0e141f780b1fe0b76d4cc8cb
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-10-27 13:21:26 +0300

    Submitting Async StandUp 27th

--- powertech/settings.py
+++ powertech/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app'
+    'main_app',
+    'shop_app',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'main_app.CustomUser'
