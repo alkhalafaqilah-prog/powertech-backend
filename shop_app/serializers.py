e5a7ba0c668c5f57bb1558eb80e4e0b677275c72
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-10-29 11:58:15 +0300

    Submitting 29th Oct UPDT2

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers 
-from .models import Product
+from .models import Product, Cart, CartItem
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -18,3 +18,15 @@ class DetailedProductSerializer(serializers.ModelSerializer):
         products = Product.objects.filter(category=product.category).exclude(id=product.id)
         serializer = ProductSerializer(products, many=True)
         return serializer.data
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart 
+        fields = ["id", "cart_code", "created_at", "modified_at"]
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    cart = CartSerializer(read_only=True)
+    class Meta:
+        model = CartItem 
+        fields = ["id", "quantity", "product", "cart"]
