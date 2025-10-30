c7fa364cd98075f0df33e2c8ccf78b4e18102305
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-10-30 12:42:49 +0300

    Created delete_cartitem view

--- shop_app/views.py
+++ shop_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from .models import Product, Cart, CartItem
 from .serializers import ProductSerializer, DetailedProductSerializer, CartSerializer, CartItemSerializer, SimpleCartSerializer
 from rest_framework.response import Response
-
+from rest_framework import status
 # Create your views here.
 
 @api_view(['GET'])
@@ -62,3 +62,27 @@ def get_cart(request):
     cart = Cart.objects.get(cart_code=cart_code, paid=False)
     serializer = CartSerializer(cart)
     return Response(serializer.data)
+
+# Here we are taking only the quantity field of our cart items
+@api_view(['PATCH'])
+def update_quantity(request):
+    try:
+        cartitem_id =  request.data.get("item_id")
+        quantity = request.data.get("quantity")
+        quantity = int(quantity)
+        cartitem = CartItem.objects.get(id=cartitem_id)
+        cartitem.quantity = quantity
+        cartitem.save()
+        serializer = CartItemSerializer(cartitem)
+        return Response({ "data":serializer.data, "message": "Cartitem updated successfully!"}, status=200)
+    
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
+    
+
+@api_view(['POST'])
+def delete_cartitem(request):
+    cartitem_id = request.data.get("item_id")
+    cartitem = CartItem.objects.get(id=cartitem_id)
+    cartitem.delete()
+    return Response({"mesaage": "Item deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
