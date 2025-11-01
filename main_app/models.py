86ffd6948e385fbe113c1ae813d44f42d5795181
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-11-01 16:01:56 +0300

    Modified CustomerUser model

--- main_app/models.py
+++ main_app/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     city = models.CharField(max_length=100,blank=True,null= True)
     state = models.CharField(max_length=100,blank=True,null= True)
     address = models.TextField(blank=True,null= True)
-    state = models.CharField(max_length=100,blank=True,null= True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
     
     def __str__(self):
         return self.username
