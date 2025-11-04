abbc8ba8ca13874cd82a4637ba5a42f8d612a0d4
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-11-04 14:50:58 +0300

    Modified UserRegistrationSerializer & register_user view

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    path("signup/", views.register_user, name="register_user"),
     path("products", views.products, name="products"),
     path("product_detail/<slug:slug>", views.product_detail, name="product_detail"),
     path("add_item/", views.add_item, name="add_item"),
