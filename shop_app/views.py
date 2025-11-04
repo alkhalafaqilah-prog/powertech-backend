d48f2de104443c1ec691c02604d937855306c437
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-11-04 13:12:35 +0300

    Created UserRegistrationSerializer & register_user view

--- shop_app/views.py
+++ shop_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from .models import Product, Cart, CartItem, Transaction
-from .serializers import ProductSerializer, DetailedProductSerializer, CartSerializer, CartItemSerializer, SimpleCartSerializer, UserSerializer
+from .serializers import ProductSerializer, DetailedProductSerializer, CartSerializer, CartItemSerializer, SimpleCartSerializer, UserSerializer, UserRegistrationSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -14,6 +14,14 @@ import requests
 
 BASE_URL = "http://localhost:5173"
 # BASE_URL = settings.REACT_BASE_URL
+
+@api_view(["POST"])
+def register_user(request):
+    serializer = UserRegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
